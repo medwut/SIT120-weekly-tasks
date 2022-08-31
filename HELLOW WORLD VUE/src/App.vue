f05e2f3106557a1c27ac1a5ee99592a69337cd88
medwut <ahmed.h.maghrabi@gmail.com>
2022-09-01 07:58:58 +1000

    Added week 4 task q1

---
+++ HELLOW WORLD VUE/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1> Hello World My name Is ahmed Maghrabi </h1>
+  <p> Within this task we created a vue project and added the hellow world message </p>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
