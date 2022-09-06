886db5f7fa63b014cbcc2e9185d6363d863d061f
medwut <ahmed.h.maghrabi@gmail.com>
2022-09-06 10:59:06 +1000

    changes

---
+++ ignore random/hello-world/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <title> Password Shower</title>
+  <h1> Hi There Please enter your Email and Password Below </h1>
+  <head>For Task 1 of Week 6 SIT120 webb responsive apps</head>
+  <body>
+    <div id="app">
+        <p> Email Name</p>
+        <input v-model="email"
+         placeholder="Please Enter Email"
+         />
+         <p>Email is: {{ email }}</p>
+        <p> Password </p>
+        <input 
+        v-model="Password"
+        type="text"
+         placeholder="Enter Password"
+         />
+         <p>Password is: {{ Password }}</p>      
+    </div>
+  </body>
+  
+</template>
+
+<script >
+   Vue.createApp({
+    el: '#app', 
+    data: {
+      email: 'radioactivewaste@gmail.com',
+      Password: 'memyselfandiarethebest41',
+    },
+  });
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ week 6 real vue stuff/main.js
@@ -0,0 +1,31 @@
+
+// Vue.createApp; {
+
+//     data(); {
+//         return{
+//       email: '',
+//       Password: '',
+//     },
+//   }
+// };
+
+Vue.createApp({
+    data() {
+      return {
+        email:'',
+        Password:''
+      }
+    }
+  }).mount('#app')
+
+
+
+// ({
+//     data() {
+//     return {
+//        {
+//         email: '',
+//         Password: '',
+//       }
+//     }
+//   }).mount('#app')
